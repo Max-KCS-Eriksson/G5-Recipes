f8e4cdfdd43467dd6e2ed71063006c56f3c05eba
Max Eriksson <teratoscincus@hotmail.com>
2025-10-04 17:58:28 +0200

    feat(api): Scaffold connection function signatures
    Non-functional at this staged.

---
+++ src/recipes/api/connection.js
@@ -0,0 +1,65 @@
+/**
+ * Fetch all `Recipe`.
+ *
+ * @async
+ * @param {string} [nameQuery] - Optionally narrow down list by search term.
+ * @returns {Array<Recipe>} All `Recipe` stored in the database.
+ */
+export async function getRecipes(nameQuery) { }
+
+/**
+ * Fetch specified `Recipe` details.
+ *
+ * @async
+ * @param {string} id - A 24 character hexadecimal.
+ * @returns {Recipe} A `Recipe` with details.
+ */
+export async function getRecipeById(id) { }
+
+/**
+ * Fetch all categories stored in the database.
+ *
+ * @async
+ * @returns {string} Category name.
+ */
+export async function getCategories() { }
+
+/**
+ * Fetch a categorized list of `Recipe`.
+ *
+ * @async
+ * @param {string} category - Name of category.
+ * @returns {Array<Recipe>} Narrowed down list of `Recipe`.
+ */
+export async function getRecipesByCategory(category) { }
+
+/**
+ * Publish a rating of a specific `Recipe`.
+ *
+ * @param {string} id - A 24 character hexadecimal, specifying rated `Recipe`.
+ * @param {number} rating - Rating of `Recipe`
+ * @returns {boolean} Success confirmation in the form of a `Boolean`.
+ */
+export async function rateRecipeById(id, rating) { }
+
+/**
+ * Fetch all `Comment` for specified `Recipe`.
+ *
+ * @async
+ * @param {string} id - A 24 character hexadecimal `Recipe` id.
+ * @returns {Array<Comment>} All stored `Comment` of the `Recipe`.
+ */
+export async function getCommentsByRecipeId(id) { }
+
+/**
+ * Publish a `Comment` for specified `Recipe`.
+ * Currently the API returns 404 if more keys other than `comment` and `name`
+ * are in the request body object.
+ *
+ * @async
+ * @param {string} id - A 24 character hexadecimal `Recipe` id.
+ * @param {string} comment - `Comment` description.
+ * @param {string} author - Name of `Comment`'s author.
+ * @returns {boolean} Success confirmation in the form of a `Boolean`.
+ */
+export async function commentRecipeById(id, comment, author) { }
