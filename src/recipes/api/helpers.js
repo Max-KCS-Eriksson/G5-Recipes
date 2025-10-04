2c46ce5f92bccdc245d829539ba1b558f4e75728
Max Eriksson <teratoscincus@hotmail.com>
2025-10-04 19:08:15 +0200

    feat(api): `isHex(s)` pass testing

--- src/recipes/api/helpers.js
+++ src/recipes/api/helpers.js
@@ -1,5 +1,8 @@
 function isHex(str) {
-  return false;
+  const pattern = /^[a-fA-F0-9]+$/;
+
+  str.startsWith("0x") ? str.slice(2) : str; // Common optional prefix
+  return pattern.test(str);
 }
 
 module.exports = { isHex };
