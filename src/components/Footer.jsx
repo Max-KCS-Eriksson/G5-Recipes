2bea2bb8524a6531873f429ed878ec6ba0ab5047
Sirak-K <ssirak@hotmail.com>
2025-10-07 02:14:55 +0200

    Använd Logo som Komponent

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,16 @@
 import "./Footer.css";
+import Logo from "./Logo.jsx";
 
 function Footer() {
   return (
     <footer className="footer-container">
       <div className="footer-content">
-        <p className="footer-logo">Pajparadiset</p>
-
+        <Logo variant="small" />
         <nav className="footer-links">
           <a href="#">Om oss</a>
           <a href="#">Kontakt</a>
           <a href="#">Integritetspolicy</a>
         </nav>
-
         <p className="footer-copy">
           © {new Date().getFullYear()} Pajparadiset. Alla rättigheter förbehållna.
         </p>
