36ac2433991f9289e6a7d217cd6988221fb2ecdc
Sirak-K <ssirak@hotmail.com>
2025-10-07 15:57:51 +0200

    Skapa Header (ny)

---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+import "./Header.css";
+import Logo from "./Logo.jsx";
+
+function Header() {
+  return (
+    <header className="header-container">
+      <img
+        className="header-bg-image"
+        src="/src/assets/images/Paj_1 (pexels-polina-tankilevitch-5419100).jpg"
+        alt="Pajbakgrund"
+      />
+      <div className="header-logo-container">
+        <Logo
+          variant="default"
+          onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+        />
+      </div>
+      <nav className="header-nav">
+        <button className="nav-btn">Knapp 1</button>
+        <button className="nav-btn">Knapp 2</button>
+        <button className="nav-btn">Knapp 3</button>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
