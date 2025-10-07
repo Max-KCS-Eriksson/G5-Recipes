f296040b516d9637051b070076329e4379f588ed
Sirak-K <ssirak@hotmail.com>
2025-10-07 02:05:22 +0200

    Gör Logo till en Component + Header Fade-animation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,20 @@
 import "./Header.css";
+import Logo from "./Logo.jsx";
 
 function Header() {
   return (
-    
     <header className="header-container">
-    <img
-    className="header-bg-image"
-    src="/src/assets/images/Paj_1 (pexels-polina-tankilevitch-5419100).jpg"
-    alt="Pajbakgrund"
-    />
-
+      <img
+        className="header-bg-image"
+        src="/src/assets/images/Paj_1 (pexels-polina-tankilevitch-5419100).jpg"
+        alt="Pajbakgrund"
+      />
       <div className="header-logo-container">
-        
-      <h1 class="header-logo">Pajparadiset</h1>  
+        <Logo
+          variant="default"
+          onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+        />
       </div>
-
       <nav className="header-nav">
         <button className="nav-btn">Knapp 1</button>
         <button className="nav-btn">Knapp 2</button>
