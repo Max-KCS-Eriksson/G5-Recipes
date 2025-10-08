a4511456a80e37143c6feb43763680859e9d793a
sandraszu <sandra.szu@iths.se>
2025-10-08 09:45:24 +0200

    chore(footer): Add Footer in Layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom";
+import Footer from "./Footer.jsx";
 
 function Layout() {
   return (
     <>
       <header></header>
       <Outlet />
-      <footer></footer>
+      <Footer />
     </>
   );
 }
