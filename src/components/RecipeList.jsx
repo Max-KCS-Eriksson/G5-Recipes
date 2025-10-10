4b2964778e43fa06a0474f8053fd492908695ee4
Max Eriksson <teratoscincus@hotmail.com>
2025-10-10 11:07:38 +0200

    feat(recipes): fetch list by category or all

--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -1,3 +1,12 @@
+import { getRecipes, getRecipesByCategory } from "../api/connection";
+
 export default function RecipeList({ category }) {
+  let recipes;
+  if (category) {
+    recipes = getRecipesByCategory(category);
+  } else {
+    recipes = getRecipes;
+  }
+
   return;
 }
