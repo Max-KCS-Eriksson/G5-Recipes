a66367220a47464030d94494bb928f56bfa4d9a2
Max Eriksson <teratoscincus@hotmail.com>
2025-10-15 15:03:26 +0200

    feat(category): `CategoryPage` MVP
    Co-authored-by: Belle Sangthong <belle.sangthong@iths.se>
    Co-authored-by: sandraszu <sandra.szu@iths.se>
    Co-authored-by: Sirak-K <ssirak@hotmail.com>

--- src/pages/CategoryPage.jsx
+++ src/pages/CategoryPage.jsx
@@ -1,19 +1,59 @@
-import { Link, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { getCategories, getRecipesByCategory } from "../api/connection";
+import CategoryList from "../components/CategoryList";
 
 function CategoryPage() {
-  const { categoryName } = useParams();
+  const [categories, setCategories] = useState([]);
+  const [categoryRecipeCount, setCategoryRecipeCount] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function fetchCategoriesAndCounts() {
+      try {
+        const categoryList = await getCategories();
+        setCategories(categoryList);
+
+        const results = await Promise.all(
+          categoryList.map(async (category) => {
+            const recipes = await getRecipesByCategory(category);
+            return { [category]: recipes.length };
+          }),
+        );
+
+        const recipeCountMap = results.reduce(
+          (acc, curr) => ({ ...acc, ...curr }),
+          {},
+        );
+
+        setCategoryRecipeCount(recipeCountMap);
+      } catch (err) {
+        console.error("Fel vid hämtning av kategorier:", err);
+        setError("Kunde inte ladda kategorier.");
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchCategoriesAndCounts();
+  }, []);
+
+  if (loading) return <p>Laddar kategorier...</p>;
+  if (error)
+    return (
+      <p style={{ color: "red" }}>
+        {error} <br /> Försök igen senare.
+      </p>
+    );
 
   return (
     <>
-      <h1>Pajkategorier</h1>
-      <p>
-        Här är receptkategori id nr: <strong>{categoryName}</strong>
-      </p>
-      <p>
-        <Link to="/">
-          <strong>Go back to Home/Main</strong>
-        </Link>
-      </p>
+      <section className="content">
+        <CategoryList
+          categories={categories}
+          categoryRecipeCount={categoryRecipeCount}
+        />
+      </section>
     </>
   );
 }
