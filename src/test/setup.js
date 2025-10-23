09b605fd6ba14859a7ceb0827f62edd161f84d22
sandraszu <sandra.szu@iths.se>
2025-10-23 12:34:03 +0200

    fix: updated to correct import for { cleanup}

--- src/test/setup.js
+++ src/test/setup.js
@@ -1,6 +1,6 @@
 import "@testing-library/jest-dom";
 import { afterEach, vi } from "vitest";
-import { cleanup } from "@testing-library/svelte";
+import { cleanup } from "@testing-library/react";
 
 afterEach(() => {
   cleanup();
