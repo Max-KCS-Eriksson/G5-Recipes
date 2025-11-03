9a32150d08fa2b0661e381701c7515c22c55bbc9
sandraszu <sandra.szu@iths.se>
2025-11-03 23:11:40 +0100

    fix(category) revert change to unified name and count to preserv previous logic

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -41,11 +41,8 @@ function Category({
           )
         }
       >
-        {/* <span className={styles.categoryName}>{name}</span>
-        <span className={styles.categoryRecipeCount}>({recipeCount ?? 0})</span> */}
-        <span className={styles.categoryLabel}>
-          {name} ({recipeCount ?? 0})
-        </span>
+        <span className={styles.categoryName}>{name}</span>
+        <span className={styles.categoryRecipeCount}>({recipeCount ?? 0})</span>
       </NavLink>
     );
   }
